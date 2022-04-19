451045b3db9756598ca3c8fa7562f58e26da647c
Andrius Pukšta <andrius.puksta@mif.stud.vu.lt>
2022-04-19 11:38:14 +0300

    use Blazor server instead of Blazor WebAssembly

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace shipping_service.Shared;
+namespace shipping_service.Data;
 
 public class WeatherForecast
 {
@@ -6,7 +6,7 @@ public class WeatherForecast
 
     public int TemperatureC { get; set; }
 
-    public string? Summary { get; set; }
-
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using shipping_service.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -23,14 +21,11 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
