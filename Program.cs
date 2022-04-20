dab3e79250b379c2c7224c947df9d60753f82119
Evaldas Visockas <vievaldas@gmail.com>
2022-04-20 13:30:12 +0300

    Place configuration in Program.cs instead of Startup.cs, Remove Startup.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
-namespace shipping_service
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using Microsoft.EntityFrameworkCore;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+using shipping_service.Entities;
+using shipping_service.Repositories;
+using shipping_service.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+// Configure services
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Services.AddDbContext<ServiceDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICourierRepository, CourierRepository>();
+builder.Services.AddScoped<IPackageRepository, PackageRepository>();
+builder.Services.AddScoped<IPostMachineRepository, PostMachineRepository>();
+builder.Services.AddScoped<ISenderRepository, SenderRepository>();
+builder.Services.AddScoped<IPackageService, PackageService>();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); This one could come in handy.
+builder.Services.AddServerSideBlazor();
+//Build
+var app = builder.Build();
+//Configure
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+app.Run();
