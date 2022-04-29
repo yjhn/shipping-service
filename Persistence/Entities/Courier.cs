f57801e31ad4e71f5e3dc4f7de114bf698e603e8
yjhn <andrius.puksta@gmail.com>
2022-04-29 12:28:17 +0300

    change IDs to be long instead of ulong, this should fix automatic ID generation

--- Persistence/Entities/Courier.cs
+++ Persistence/Entities/Courier.cs
@@ -5,7 +5,7 @@
         public string Username { get; set; }
         public byte[] HashedPassword { get; set; }
         public ICollection<Shipment> CurrentShipments { get; set; }
-        public ulong Id { get; set; }
+        public long Id { get; set; }
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
     }
--- Persistence/Entities/PostMachine.cs
+++ Persistence/Entities/PostMachine.cs
@@ -6,7 +6,7 @@
         public string Address { get; set; }
         public ICollection<Shipment> ShipmentsWithThisSource { get; set; } = new List<Shipment>();
         public ICollection<Shipment> ShipmentsWithThisDestination { get; set; } = new List<Shipment>();
-        public ulong Id { get; set; }
+        public long Id { get; set; }
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
     }
