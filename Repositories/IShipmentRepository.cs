7e4f28a00a91cd52082b4250583acd8cc51cef8f
yjhn <andrius.puksta@gmail.com>
2022-05-23 14:10:19 +0300

    create GetAsync method for getting shipment by id

--- Repositories/IShipmentRepository.cs
+++ Repositories/IShipmentRepository.cs
@@ -5,6 +5,7 @@ namespace shipping_service.Repositories
     public interface IShipmentRepository
     {
         IQueryable<Shipment> Shipments { get; }
+        Task<Shipment?> GetAsync(long id);
 
         Task CreateAsync(Shipment shipment);
 
