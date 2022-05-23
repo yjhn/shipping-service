87294a0d3f55f80312001d54c87fe2069ed31d31
yjhn <andrius.puksta@gmail.com>
2022-05-23 09:46:48 +0300

    delete duplicate methods mistakenly left from the rebase

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -164,40 +164,6 @@ namespace shipping_service.Services
             return ExistsSender() || ExistsCourier() ? true : false;
         }
 
-        private byte[] PasswordHash(string password)
-        {
-            byte[] salt = new byte[16];
-            new RNGCryptoServiceProvider().GetBytes(salt);
-
-            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000);
-            byte[] hash = pbkdf2.GetBytes(20);
-
-            byte[] hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
-
-            return hashBytes;
-        }
-
-        private bool ValidatePasswordHash(string password, byte[] dbPasswordHash)
-        {
-            byte[] salt = new byte[16];
-            Array.Copy(dbPasswordHash, 0, salt, 0, 16);
-
-            Rfc2898DeriveBytes userPasswordBytes = new Rfc2898DeriveBytes(password, salt, 1000);
-            byte[] userPasswordHash = userPasswordBytes.GetBytes(20);
-
-            for (int i = 0; i < 20; i++)
-            {
-                if (dbPasswordHash[i + 16] != userPasswordHash[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
         private string LoginBegin(string password)
         {
             if (!ExistsCourier() && !ExistsSender())
